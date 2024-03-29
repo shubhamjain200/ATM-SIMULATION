c47c00feb6cac078e6dd8a1211a9cd4ad5fe2de0
shubhamjain200 <99166944+shubhamjain200@users.noreply.github.com>
2024-03-29 13:11:19 +0530

    Add files via upload

---
+++ AtmSimulator/src/pack/cash/ATM.java
@@ -0,0 +1,14 @@
+package pack.cash;
+
+/* Main Class
+@Author Yu Lin - CSCI 185 - SPRING 2015 - ATM Project */
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+}
